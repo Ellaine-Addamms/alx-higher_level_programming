79425e356cd8375fde2c7b263bb56febd75f1b2d
Ellaine-Addamms <ellaine.addamms@gmail.com>
2023-10-22 04:56:41 -0700

    new

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
-def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School"""
-    from magic_calculation_102 import add, sub
-
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-    else:
-        return(sub(a, b))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
